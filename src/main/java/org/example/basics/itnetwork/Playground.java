98d4263fc9a1c05d19112abda9a25741db1c1bb8
Matěj <mckillem@tuta.io>
2025-07-29 15:33:02 +0200

    3-5

--- src/main/java/org/example/basics/itnetwork/Playground.java
+++ src/main/java/org/example/basics/itnetwork/Playground.java
@@ -1,7 +1,30 @@
 package org.example.basics.itnetwork;
 
+import java.util.Scanner;
+
 public class Playground {
     public static void basics() {
-        System.out.println("Hello World");
+        triAzPet();
+    }
+
+    public static void triAzPet() {
+//        System.out.println("Ahoj, jak se jmenuješ?");
+//        Scanner scanner = new Scanner(System.in);
+//        String jmeno = scanner.nextLine();
+//        System.out.println("Jaký jsi?");
+//        String vlastnost = scanner.nextLine();
+//        System.out.println(jmeno + " je "  + vlastnost);
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Zadej číslo k umocnění: ");
+//        int cislo = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Výsledek: " + cislo * cislo);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadej poloměr kruhu (cm): ");
+        double polomer = Double.parseDouble(scanner.nextLine());
+        System.out.println("Obvod zadaného kruhu je: " + 2*Math.PI*polomer + " cm");
+        System.out.println("Jeho obsah je: " + Math.PI*Math.pow(polomer, 2) + " cm^2");
     }
 }
