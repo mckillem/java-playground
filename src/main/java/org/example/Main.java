4debb7b95785596b61b20b08ba74df42a7533335
Matěj <mckillem@tuta.io>
2025-08-01 17:26:04 +0200

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.example.basics.itnetwork.Playground;
+import org.example.basics.ljthw.Playground2;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       Playground.basics();
+//       Playground.basics();
+        Playground2.basics();
     }
 }
---
+++ src/main/java/org/example/basics/ljthw/Playground2.java
@@ -0,0 +1,14 @@
+package org.example.basics.ljthw;
+
+public class Playground2 {
+	public static void basics() {
+		String[] args = {"to", "ko"};
+
+		first(args);
+	}
+
+	public static void first(String[] args) {
+		System.out.println("I am determined to learn how to code.");
+		System.out.println("Today's date is");
+	}
+}
