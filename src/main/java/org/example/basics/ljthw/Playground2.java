c2c3b25143360553dd57c6234db8e59c43f9e9f4
Matěj <mckillem@tuta.io>
2025-08-01 17:53:26 +0200

    third

--- src/main/java/org/example/basics/ljthw/Playground2.java
+++ src/main/java/org/example/basics/ljthw/Playground2.java
@@ -4,10 +4,15 @@ public class Playground2 {
 	public static void basics() {
 		String[] args = {"to", "ko"};
 
-		first(args);
+		third(args);
 	}
 
 	public static void first(String[] args) {
+		System.out.println("I am determined to learn how to code.");
+		System.out.println("Today's date is");
+	}
+
+	public static void third(String[] args) {
 		System.out.println("I am determined to learn how to code.");
 		System.out.println("Today's date is");
 	}
