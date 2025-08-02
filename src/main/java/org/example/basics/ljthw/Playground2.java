4f0c9639ff7519eab60e769a0884328cfe6a9b56
Matěj <mckillem@tuta.io>
2025-08-02 14:27:24 +0200

    third

--- src/main/java/org/example/basics/ljthw/Playground2.java
+++ src/main/java/org/example/basics/ljthw/Playground2.java
@@ -1,5 +1,7 @@
 package org.example.basics.ljthw;
 
+import java.util.Scanner;
+
 public class Playground2 {
 	public static void basics() {
 		String[] args = {"to", "ko"};
@@ -13,7 +15,21 @@ public class Playground2 {
 	}
 
 	public static void third(String[] args) {
-		System.out.println("I am determined to learn how to code.");
-		System.out.println("Today's date is");
+//		Scanner scanner = new Scanner(System.in);
+//		System.out/* hjdjdjdkdls */.println("I am determined to learn how to code.");
+//		String date = scanner.nextLine();
+//		System.out.println("Today's date is " + date);
+
+		int age = 22;
+		boolean onGuestList = false;
+		double allure = 7.5;
+		String gender = "F";
+
+		if ( onGuestList || age >= 21 || (gender.equals("F") && allure >= 8) ) {
+			System.out.println("You are allowed to enter the club.");
+			} else {
+				System.out.println("You are not allowed to enter the club.");
+			}
 	}
 }
+//fjfjf
